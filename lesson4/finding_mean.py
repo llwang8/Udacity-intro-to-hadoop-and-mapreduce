c89163d3a38649b823516155961ef489a835de51
llwang8 <wangxbl56@gmail.com>
2016-09-29 10:35:08 -0400

    fixed lesson.4 finding_mean error of %Y

--- lesson4/finding_mean.py
+++ lesson4/finding_mean.py
@@ -3,7 +3,10 @@
 # Lesson 4: MapReduce Design Pattern   #
 # -------------------------------------#
 # Quiz: Finding Mean
-from datatime import datatime
+
+#!/usr/bin/python
+
+from datetime import datetime
 import sys
 
 def mapper():
@@ -14,7 +17,7 @@ def mapper():
             continue
 
         date, time, store, item, cost, payment = data
-        weekday = datetime.strptime(date, "%y-%m-%d").weekday()
+        weekday = datetime.strptime(date, "%Y-%m-%d").weekday()
         print "{0}\t{1}".format(weekday, cost)
 
 
@@ -31,7 +34,7 @@ def reducer():
         weekday, sales = data_mapped
         sales = float(sales)
 
-        sales_summary = sales_by_weekday.get(weekday, {'sum': 0, 'count': 0})
+        sales_summary = sales_by_weekday.get(weekday, {'sum': 0, 'count': 0})  # using get to provide a default if not exists
         sales_summary['sum'] += sales
         sales_summary['count'] += 1
         sales_by_weekday[weekday] = sales_summary
@@ -41,6 +44,7 @@ def reducer():
 
 
 
-
-
+purchases = ['2012-01-01\t09:00\tSan Jose\tMens clothing\t214.05\tAmex',
+                '2012-01-01\t09:00\tFort Worth\tWomen clothing\t15.57\tVisa'
+            ]
 
